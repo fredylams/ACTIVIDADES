59305b5f55c85de2bf3bfb6e693e64ebc280f1b4
fredylams <82283487+fredylams@users.noreply.github.com>
2021-04-15 16:25:20 -0500

    Create DerivadaconformulageneralLuisAlfredoMartinezSantiago3201.m

---
+++ DerivadaconformulageneralLuisAlfredoMartinezSantiago3201.m
@@ -0,0 +1,33 @@
+% FuncionLineal
+% Título: Funcion constante
+% Descripción: Script para graficar una funcion de la forma 
+% Autor: LUIS ALFREDO MARTINEZ SANTIAGO (FREDYLAMS) fredsantiago279@gmail.com
+% Fecha: 20210414
+% Sesion: 1
+% De uso: octava> / ruta / 
+% Notas: Requiere aplicacion octava usar en consola preferiblemente
+
+% y=((x-(-2)))
+% si se graficá su comportamiento es lineal
+% ^
+% y |
+% |   
+% | ____________
+% | 
+% | _____________>x
+%
+
+% limpiar variables
+ clear
+% Ejemplo f (x) = a
+% Rango de -3..3 en i = 0.1
+ x = [-3: 1 :3]; 
+% Valor de la función 
+ y=((x-(-2)))
+% Dibujar x, y
+ plot(x , y);
+% Titulo ("f (x)=((x-(-2)))
+% Etiqueta para x
+ xlabel ( "x");
+% Etiqueta para y
+ylabel ("y");
